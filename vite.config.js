9ad4bc2555a8a03b24473b6d828a77210127f027
Paul156551 <pavel.ryzhkov.99@mail.ru>
2025-06-24 15:23:18 +0300

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/cryptoplatform",
   resolve: {
     alias: {
       // Example alias if needed
